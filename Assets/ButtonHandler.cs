5c8cd3793fc96b3cfa0a5381b08d547b1bceb15f
Aleksi Kalandia <alexikaland@gmail.com>
2022-04-15 21:19:26 +0300

    Login screen

---
+++ Assets/ButtonHandler.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    [SerializeField] private ButtonHandler _button; 
+}
